ddeba2233f611802024eb95bbd501765e69b5a84
FreezyPZ <99330340+FreezyPZ@users.noreply.github.com>
2022-04-04 18:28:30 +0200

    esercizio 10

---
+++ 10-blocchi condizionali e cicli/exercise-7.js
@@ -0,0 +1,11 @@
+function sumUntil(maxValue) {
+  let summ = 0;
+  for(let i = 0; i <= maxValue; i++){
+    summ = summ + i;
+    console.log(i, "+")
+  }
+  console.log("__")
+  console.log(summ);
+}
+
+console.log(sumUntil(10));
