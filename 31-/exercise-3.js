c0998122c0bb8c00e2d32c244df8b4c500b61c8b
FreezyPZ <99330340+FreezyPZ@users.noreply.github.com>
2022-04-26 11:22:55 +0200

    es 29-30-31-34

---
+++ 31-/exercise-3.js
@@ -0,0 +1,22 @@
+function calculateAverageAge(persons) {
+  const age = persons.map(person => person.age)
+  const sum = age.reduce((a, b) => a + b, 0);
+  return sum / age.length;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
