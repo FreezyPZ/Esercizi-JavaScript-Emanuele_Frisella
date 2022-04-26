7094ae38b51ea1c0117412119c1fbe83f1095556
FreezyPZ <99330340+FreezyPZ@users.noreply.github.com>
2022-04-26 15:28:49 +0200

    es 32-33-36

---
+++ 32-/exercise-4.js
@@ -0,0 +1,63 @@
+function uncompletedNotes(notes) {
+  const uncompletedNotes = [];
+  notes.forEach(note => {
+    note.todos.forEach(todo => {
+      if (todo.done === false) uncompletedNotes.push(note);
+    } )
+  });
+  return uncompletedNotes;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: 'Workout program',
+    todos: [
+      {
+        id: 1,
+        name: 'Push ups - 10 x 3',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Abdominals - 20 x 3',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Tapis Roulant - 15min',
+        done: true
+      }
+    ]
+  },
+  {
+    id: 2,
+    description: 'Front-end Roadmap',
+    todos: [
+      {
+        id: 1,
+        name: 'Learn HTML',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Learn CSS',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Learn JavaScript',
+        done: true
+      },
+      {
+        id: 4,
+        name: 'Learn Angular',
+        done: true 
+      }
+    ]
+  }
+]
+
+const notesInProgress = uncompletedNotes(notes);
+console.log('All notes: ', notes);
+console.log('Notes In Progress: ', notesInProgress);
