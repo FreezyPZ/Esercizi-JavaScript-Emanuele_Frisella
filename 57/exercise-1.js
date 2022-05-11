e8ee27c9c808c410f161106ba71fc7bcfe009e5a
FreezyPZ <99330340+FreezyPZ@users.noreply.github.com>
2022-05-11 20:05:53 +0200

    es n. 57-58-59-60-61-62-63-64

---
+++ 57/exercise-1.js
@@ -0,0 +1,35 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    const person = persons.find((person) => person.id === id)
+    if(person){
+      resolve (person);
+    }
+    else{
+      reject( new Error("not found"));
+    }
+  })
+}
+fetchPersonById(1)
+.then(person => console.log(person))
+.catch((error) => console.log(error)); 
---
+++ 61/exercise-5.js
@@ -0,0 +1,72 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+// core here
+
+function fetchPersonById(id){
+  return new Promise((resolve,reject)=> {
+    setTimeout(()=> {
+      const person = persons.find((person)=> person.id === id)
+      if(person){
+        resolve(person)
+      }else{
+        reject(new error('Person not found'))
+      }
+    }, 1000)
+  })
+}
+
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      const job = jobs.find((element)=> element.id === id)
+      if(job){
+        resolve(job)
+      }else{
+        reject(new Error("Job not found"))
+      }
+    }, 500)
+  })
+}
+
+
+Promise.race([
+  fetchPersonById(2),
+  fetchJobById(3)
+])
+  .then((firstItemReturned) => console.log(firstItemReturned))
+  .catch((firstErrorReturned) => console.log(firstErrorReturned));
